d11e9bd0961ede1be2ee55bd082fcbd85975c806
patryk.lorbiecki1 <patryk.lorbiecki1@gmail.com>
2025-08-04 20:05:48 +0200

    test: add unit tests for register method

---
+++ src/test/java/com/example/appointment_booking/AuthServiceTest.java
@@ -0,0 +1,102 @@
+package com.example.appointment_booking;
+
+import com.example.appointment_booking.application.AuthService;
+import com.example.appointment_booking.config.security.JwtUtil;
+import com.example.appointment_booking.domain.exception.UsernameAlreadyExistsException;
+import com.example.appointment_booking.domain.model.Role;
+import com.example.appointment_booking.domain.model.Specialist;
+import com.example.appointment_booking.domain.model.User;
+import com.example.appointment_booking.domain.repository.SpecialistRepository;
+import com.example.appointment_booking.domain.repository.UserRepository;
+import com.example.appointment_booking.web.dto.RegisterRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private SpecialistRepository specialistRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtUtil jwtUtil;
+    @InjectMocks
+    private AuthService authService;
+
+    @Test
+    void register_shouldThrow_whenUsernameExists(){
+        RegisterRequest request = new RegisterRequest();
+        request.setUsername("adam");
+        request.setPassword("secret");
+        request.setRole(Role.PATIENT);
+
+        when(userRepository.findByUsername("adam")).thenReturn(Optional.of(new User()));
+
+        assertThrows(UsernameAlreadyExistsException.class,()->
+                authService.register(request));
+    }
+
+    @Test
+    void register_shouldRegister_whenUserNameIsUnique(){
+        RegisterRequest request = new RegisterRequest();
+        request.setUsername("jan");
+        request.setPassword("secret");
+        request.setRole(Role.PATIENT);
+
+        when(userRepository.findByUsername("jan")).thenReturn(Optional.empty());
+        when(passwordEncoder.encode("secret")).thenReturn("encoded");
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+
+        String result = authService.register(request);
+
+        verify(userRepository).save(userCaptor.capture());
+        User savedUser = userCaptor.getValue();
+
+        assertEquals("jan",savedUser.getUsername());
+        assertEquals("encoded",savedUser.getPassword());
+        assertEquals(Role.PATIENT,savedUser.getRole());
+        assertEquals("User registered",result);
+    }
+
+    @Test
+    void register_shouldRegisterSpecialist_whenRoleIsSpecialist(){
+        RegisterRequest request = new RegisterRequest();
+        request.setUsername("jan");
+        request.setPassword("secret");
+        request.setRole(Role.SPECIALIST);
+
+        when(userRepository.findByUsername("jan")).thenReturn(Optional.empty());
+        when(passwordEncoder.encode("secret")).thenReturn("encoded");
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        ArgumentCaptor<Specialist> specialistCaptor = ArgumentCaptor.forClass(Specialist.class);
+        String result = authService.register(request);
+
+        verify(userRepository).save(userCaptor.capture());
+        verify(specialistRepository).save(specialistCaptor.capture());
+        User savedUser = userCaptor.getValue();
+        Specialist savedSpecialist = specialistCaptor.getValue();
+
+        assertEquals("jan",savedUser.getUsername());
+        assertEquals("encoded",savedUser.getPassword());
+        assertEquals(Role.SPECIALIST,savedUser.getRole());
+        assertEquals(savedUser,savedSpecialist.getUser());
+        assertEquals("",savedSpecialist.getSpecialization());
+        assertEquals("User registered",result);
+    }
+
+}
